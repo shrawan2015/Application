f9279fba9c9a6a12afa71eb343ac43eaf49e80da
shrawan2015 <shrawan25.sharma@gmail.com>
2016-10-17 22:51:30 +0530

    WhatsApp Application

---
+++ whatsApp/whatsApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  whatsApp
+//
+//  Created by SKS on 17/10/16.
+//  Copyright © 2016 whatsApp. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        collectionView?.backgroundColor = .green
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
